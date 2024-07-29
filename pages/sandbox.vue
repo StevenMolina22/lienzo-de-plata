6e60eb789f7212b5f0924987f1f6e874e9251084
steven <stevenmolina2205@gmail.com>
2024-07-29 18:42:10 -0300

    Sandbox changes

---
+++ pages/sandbox.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello world from sandbox',
+            counter: 0,
+        }
+    }
+}
+
+</script>
+<template>
+    <div>
+        <div>{{ message }}</div>
+    <div>{{ counter }}</div>
+    <button @click="counter++">increment</button>
+    <button @click="counter--">decrement</button>
+    <button @click="counter = 10">set to 10</button>
+    <button @click="counter = 0">reset</button>
+    </div>
+</template>
